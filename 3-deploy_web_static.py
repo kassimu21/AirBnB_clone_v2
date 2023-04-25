fed34126d2bf64dc18559e75a2f526c937e2cc09
kassimu21 <sksallah45@gmail.com>
2023-04-25 11:40:18 -0700

    AirBnB_clone_v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['54.86.220.207', '54.175.137.217']
+env.hosts = ['52.201.160.66', '100.25.46.235']
 
 
 @runs_once
